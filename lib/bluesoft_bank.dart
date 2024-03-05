3191aefdd101ab1389246573fbab72c6105eb096
Mario Mederos <carlosmariomederos1@gmail.com>
2024-03-05 00:14:39 -0500

    add navigation class

--- lib/bluesoft_bank.dart
+++ lib/bluesoft_bank.dart
@@ -1,11 +1,20 @@
+import 'package:bluesoft_bank/core/di/di.dart';
 import 'package:bluesoft_bank/utils/navigation/navigation.dart';
+import 'package:bluesoft_bank/views/login/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:go_router/go_router.dart';
+import 'ui/theme/theme.dart' as bs_theme;
 
 class BluesoftBank extends StatelessWidget {
   const BluesoftBank({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      initialRoute: LoginPage.route,
+      theme: bs_theme.Theme.data,
+      onGenerateRoute: getIt<Navigation>().generateRoute,
+    );
   }
 }
--- lib/utils/navigation/navigation.dart
+++ lib/utils/navigation/navigation.dart
@@ -1,10 +1,13 @@
 import 'package:bluesoft_bank/utils/extension/widget/widget_extension.dart';
+import 'package:bluesoft_bank/views/login/login_page.dart';
 import 'package:flutter/material.dart';
 
 class Navigation {
-  static Route<dynamic> generateRoute(RouteSettings settings) {
+  Route<dynamic> generateRoute(RouteSettings settings) {
     final arguments = settings.arguments;
     switch (settings.name) {
+      case LoginPage.route:
+        return const LoginPage().buildPageRoute();
       default:
         return const Scaffold(
           body: Center(
@@ -13,98 +16,6 @@ class Navigation {
             ),
           ),
         ).buildPageRoute();
-      // case splashRoute:
-      //   return const SplashScreen().buildPageRoute();
-
-      // case wrapperRoute:
-      //   return Wrapper().buildPageRoute();
-
-      // case loginRoute:
-      //   return LoginScreen().buildPageRoute();
-      // case homeRoute:
-      //   return const HomeScreen().buildPageRoute();
-
-      // case infusionCheckRoute:
-      //   final map = arguments as Map;
-      //   Encounters encounter = map['encounter'];
-      //   int? index = map['index'];
-      //   GlobalKey? key = map['key'];
-      //   return InfusionCheckScreen(
-      //     encounter: encounter,
-      //     index: index,
-      //     key: key,
-      //   ).buildPageRoute();
-
-      // case infoStepRoute:
-      //   return InfoStep().buildPageRoute();
-
-      // case weightStepRoute:
-      //   return WeightStep().buildPageRoute();
-
-      // case insuranceStepRoute:
-      //   return InsuranceStep().buildPageRoute();
-
-      // case patientNotesStepRoute:
-      //   return PatientsNotes(
-      //     isCheck: true,
-      //   ).buildPageRoute();
-
-      // case reviewStepRoute:
-      //   final map = arguments as Map;
-      //   return ReviewNotes(
-      //     values: map["values"],
-      //     key: reviewKey,
-      //   ).buildPageRoute();
-
-      // case calculateDosageRoute:
-      //   final map = arguments as Map;
-      //   Encounters encounter = map['encounter'];
-      //   return MaterialPageRoute(
-      //     builder: (context) => PreInfusionProcessScreen(
-      //       encounter: encounter,
-      //       retake: false,
-      //     ),
-      //   );
-
-      // case retakeInfusionRoute:
-      //   final map = arguments as Map;
-      //   Encounters encounter = map['encounter'];
-      //   return MaterialPageRoute(
-      //     builder: (context) => PreInfusionProcessScreen(
-      //       encounter: encounter,
-      //       retake: true,
-      //     ),
-      //   );
-
-      // case infusionProcessRoute:
-      //   final map = arguments as Map;
-      //   Encounters encounter = map['encounter'];
-      //   Results result = map['result'];
-      //   Infusion infusion = map['infusion'];
-
-      //   int? index = map['index'];
-      //   return InfusionProcess(
-      //     encounter: encounter,
-      //     cloudResults: result,
-      //     infusion: infusion,
-      //     index: index,
-      //   ).buildPageRoute();
-
-      // case preloadInfusionSummaryRoute:
-      //   final map = arguments as Map;
-      //   Encounters encounter = map['encounter'];
-      //   return PreloadInfusionSummary(
-      //     encounter: encounter,
-      //   ).buildPageRoute();
-
-      // default:
-      //   return Scaffold(
-      //     body: Center(
-      //       child: Container(
-      //         child: Text("Wrong route"),
-      //       ),
-      //     ),
-      //   ).buildPageRoute();
     }
   }
 }
