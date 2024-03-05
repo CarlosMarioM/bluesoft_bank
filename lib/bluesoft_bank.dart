603196ee3d9069b5c83d37f7c98f1dbc83aa57ce
Mario Mederos <carlosmariomederos1@gmail.com>
2024-03-05 11:56:33 -0500

    remove go router init material app

--- lib/bluesoft_bank.dart
+++ lib/bluesoft_bank.dart
@@ -1,10 +1,8 @@
 import 'package:bluesoft_bank/core/di/di.dart';
 import 'package:bluesoft_bank/utils/navigation/navigation.dart';
 import 'package:bluesoft_bank/views/login/login_page.dart';
+import 'ui/theme/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
-import 'package:go_router/go_router.dart';
-import 'ui/theme/theme.dart' as bs_theme;
 
 class BluesoftBank extends StatelessWidget {
   const BluesoftBank({super.key});
@@ -13,7 +11,8 @@ class BluesoftBank extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: LoginPage.route,
-      theme: bs_theme.Theme.data,
+      themeMode: ThemeMode.dark,
+      theme: Theming.data,
       onGenerateRoute: getIt<Navigation>().generateRoute,
     );
   }
